9ad90ef5c4e6142ec85ce17c0fc4d1638b51e215
kasturi sathvika <kasturisathwika@gmail.com>
2026-06-15 16:37:03 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pickle
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+
+model=load_model('next_word_lstm.h5')
+with open('tokenizer.pickle','rb') as handle:
+    tokenizer=pickle.load(handle)
+    
+def prediction_next_word(model,tokenizer,text,max_len):
+    token_list=tokenizer.texts_to_sequences([text])[0]
+    if len(token_list)>=max_len:
+        token_list=token_list[-(max_sequence_len-1):]
+    token_list=pad_sequences([token_list],maxlen=max_sequence_len-1,padding='pre')
+    predicted=model.predict(token_list,verbose=0)
+    predicted_word_index=np.argmax(predicted, axis=1)
+    for word,index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+    return None    
+## strealit app
+import streamlit as st
+st.title("Next word prediction with LSTM and Early stopping")
+input_text=st.text_input("enter the sequence of words","to be or not to")
+if st.button("predict next word"):
+    max_sequence_len=model.input_shape[1]+1
+    next_word=prediction_next_word(model,tokenizer,input_text,max_sequence_len)
+    st.write(f'nextword:{next_word}')
